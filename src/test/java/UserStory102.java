d9786241562d6c5d59f0d903b6367fce87d2e7b1
rasimcagirici <82914678+rasimcagirici@users.noreply.github.com>
2021-06-30 21:28:30 -0400

    Us102 has been completed (#2)
    * Started User Story 102
    
    * Started User Story 102 rename
    
    * User Story 102 Ended
    
    * made changes

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,8 @@
+public class UserStory102 {
+
+    //lots of code has been written here
+    //ready to push this branch to remote
+    //and open pull request so I can start collaborating
+    //more changes added as requested
+
+}
